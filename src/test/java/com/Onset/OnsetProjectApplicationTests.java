85b5684cb1905da3ead12c4fbd3b2e06a8515b73
sungchae <sungge0661@naver.com>
2025-10-27 09:18:50 +0900

    Initial commit

---
+++ src/test/java/com/Onset/OnsetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Onset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
